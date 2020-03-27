0080678aa32b5d957bea3b03fb83d7d9413b3e66
Thomas <thomas.lachieze@gmail.com>
2020-03-27 16:59:59 +0100

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,7 @@
 
 function capitalizeFirstLetters(str) {
-
+    if (typeof str !== 'string') return ''
+    return str.split(' ').map(word => word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
 }
 
 
