88df1c50e57508f79ac8dcf7b4082736fa251511
Thomas <thomas.lachieze@gmail.com>
2020-03-27 16:34:26 +0100

    Writting the tests

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,14 @@
+
+function capitalizeFirstLetters(str) {
+
+}
+
+
+
+const assert = require('assert');
+
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+assert.strictEqual(capitalizeFirstLetters(''), '');
