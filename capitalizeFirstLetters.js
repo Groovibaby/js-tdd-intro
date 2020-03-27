bb8b57beedacc9375033c218106bc80f456158ff
Thomas <thomas.lachieze@gmail.com>
2020-03-27 17:21:34 +0100

    git tag step-2

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -4,12 +4,4 @@ function capitalizeFirstLetters(str) {
     return str.split(' ').map(word => word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
 }
 
-
-
-const assert = require('assert');
-
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-assert.strictEqual(capitalizeFirstLetters(''), '');
+module.exports = capitalizeFirstLetters;
